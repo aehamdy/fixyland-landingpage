5b2f75744de80d8f56f34c74135cfc45dcde6f20
AhmedEssam <ae.devverse@gmail.com>
2025-08-22 20:42:39 +0300

    Create info data file

---
+++ src/config/info.ts
@@ -0,0 +1,14 @@
+export const info = {
+  company: {
+    name: "FixyLand",
+    phone: "[+1] 987 654 3210",
+    email: "info@domain.com",
+    address: "7631 Sabina Park, 115 Deven Isle, Lousiana, USA",
+  },
+  socials: {
+    facebook: "https://www.facebook.com",
+    instagram: "https://www.instagram.com",
+    x: "https://twitter.com",
+    behance: "https://www.behance.net",
+  },
+};
