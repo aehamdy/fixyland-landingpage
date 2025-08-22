8760c788f318d2354e104c13363d28b16aff9be2
AhmedEssam <ae.devverse@gmail.com>
2025-08-22 20:32:17 +0300

    Create UserActions component

---
+++ src/components/UserActions.tsx
@@ -0,0 +1,21 @@
+import Icon from "./common/Icon";
+
+function UserActions() {
+  return (
+    <div className="flex justify-between items-center gap-4">
+      <button className="p-2 bg-white/20 text-white rounded-full hover:scale-110 transition-all cursor-pointer">
+        <Icon name="magnifying-glass" />
+      </button>
+
+      <button className="flex md:hidden p-2 bg-white/20 text-white rounded-full hover:scale-110 transition-all cursor-pointer">
+        <Icon name="menu" />
+      </button>
+
+      <button className="hidden md:flex items-center gap-2 py-sm px-sm text-content-dark bg-accent-secondary rounded-medium hover:scale-105 transition-all cursor-pointer">
+        Book Your Stay <Icon name="arrow-up-right" />
+      </button>
+    </div>
+  );
+}
+
+export default UserActions;
