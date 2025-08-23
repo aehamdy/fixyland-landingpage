1a27563cf8dcabbacf22a2daaf4a82ddec921b63
AhmedEssam <ae.devverse@gmail.com>
2025-08-23 23:11:38 +0300

    Feat: Create foodList data file

---
+++ src/config/foodList.ts
@@ -0,0 +1,26 @@
+export type FoodType = {
+  id: number;
+  title: string;
+  description: string;
+};
+
+export const foodList: FoodType[] = [
+  {
+    id: 1,
+    title: "Free Breakfast, Tea & Coffee",
+    description:
+      "Breakfast is a meal that is typically eaten in the morning, often consisting of a variety of foods such as bread, cereal, eggs, and fruit.",
+  },
+  {
+    id: 2,
+    title: "Quality Foods & Drink",
+    description:
+      "Breakfast is a meal that is typically eaten in the morning, often consisting of a variety of foods such as bread, cereal, eggs, and fruit.",
+  },
+  {
+    id: 3,
+    title: "Restaurant & Bar",
+    description:
+      "Breakfast is a meal that is typically eaten in the morning, often consisting of a variety of foods such as bread, cereal, eggs, and fruit.",
+  },
+];
