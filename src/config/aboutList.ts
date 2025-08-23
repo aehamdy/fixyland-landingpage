01696c3c3801a7b27d683b1643dbc1a4678654fb
AhmedEssam <ae.devverse@gmail.com>
2025-08-23 04:19:03 +0300

    Create aboutList data file

---
+++ src/config/aboutList.ts
@@ -0,0 +1,31 @@
+type AboutListProps = {
+  id: number;
+  label: string;
+};
+
+export const aboutList: AboutListProps[] = [
+  {
+    id: 1,
+    label: "24-Hours Emergency Service",
+  },
+  {
+    id: 2,
+    label: "Comforable Atmosphere",
+  },
+  {
+    id: 3,
+    label: "Something by accident",
+  },
+  {
+    id: 4,
+    label: "It is a Long Established Fact",
+  },
+  {
+    id: 5,
+    label: "Flexible and Cost-Effective",
+  },
+  {
+    id: 6,
+    label: "Get Free Consultation Anytime",
+  },
+];
