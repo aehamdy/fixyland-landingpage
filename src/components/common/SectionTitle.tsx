ca0934e426590ef3fd1119bf764ace8968fa5522
AhmedEssam <ae.devverse@gmail.com>
2025-08-24 21:59:27 +0300

    Style: Adjust styles

--- src/components/common/SectionTitle.tsx
+++ src/components/common/SectionTitle.tsx
@@ -6,7 +6,7 @@ type SectionHeadingProps = {
 
 function SectionTitle({ title }: SectionHeadingProps) {
   return (
-    <div className="w-fit py-1 px-1 md:px-2 font-bold text-tiny md:text-small tracking-wider text-accent-primary uppercase bg-white rounded-medium">
+    <div className="w-fit py-1 px-1 md:px-2 font-bold text-tiny md:text-small tracking-wider text-accent-primary uppercase bg-white shadow-md rounded-medium">
       <Heading level="h4" title={title} />
     </div>
   );
