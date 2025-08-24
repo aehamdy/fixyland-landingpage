4293d328a7f174dce3665c4e97e4a723a4332d4f
AhmedEssam <ae.devverse@gmail.com>
2025-08-24 22:02:25 +0300

    Style: Adjust the position

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -10,8 +10,8 @@ function NavMenu({ isNavMenuOpen }: NavMenuProps) {
     <div
       className={`flex md:hidden absolute min-w-[150px] w-2/5 end-5xl top-[calc(100%+0.5rem)] bg-surface-light rounded-md overflow-hidden ${
         isNavMenuOpen
-          ? "top-[calc(100%+1rem)] opacity-100 z-40"
-          : "opacity-0 z-[-1]"
+          ? "top-[calc(100%+1rem)] opacity-100 z-40 pointer-events-auto"
+          : "opacity-0 pointer-events-none"
       } transition-all duration-normal`}
     >
       <ul className="w-full">
