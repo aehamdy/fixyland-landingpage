ca3986e721d1e4ad4483ad4e60633da9fbb7fff9
AhmedEssam <ae.devverse@gmail.com>
2025-08-24 21:48:54 +0300

    Feat: Create exploreList data file

---
+++ src/config/exploreList.ts
@@ -0,0 +1,33 @@
+export interface ExploreListProps {
+  id: number;
+  label: string;
+  link: string;
+}
+
+export const exploreList: ExploreListProps[] = [
+  {
+    id: 1,
+    label: "About Hotel",
+    link: "#",
+  },
+  {
+    id: 2,
+    label: "Pricing",
+    link: "#",
+  },
+  {
+    id: 3,
+    label: "Hotel Staff",
+    link: "#",
+  },
+  {
+    id: 4,
+    label: "Latest News",
+    link: "#",
+  },
+  {
+    id: 5,
+    label: "Contact Us",
+    link: "#",
+  },
+];
