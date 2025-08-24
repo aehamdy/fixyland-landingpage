aa9d276a827da8a4b53249361702e035bd6e6083
AhmedEssam <ae.devverse@gmail.com>
2025-08-24 21:54:34 +0300

    Chore: Add mode and isScalable props

--- src/components/common/Logo.tsx
+++ src/components/common/Logo.tsx
@@ -1,10 +1,17 @@
 import { info } from "../../config/info";
 
-function Logo() {
+type LogoProps = {
+  mode: "dark" | "light";
+  isScalable?: boolean;
+};
+
+function Logo({ mode, isScalable }: LogoProps) {
   return (
     <a
       href="#"
-      className="flex justify-center items-center font-bold md:text-medium lg:text-3xl text-content-dark bg-white hover:scale-110 transition-all"
+      className={`flex justify-center items-center font-bold md:text-medium lg:text-3xl ${
+        mode === "dark" ? "text-content-dark" : "text-content-light"
+      } ${isScalable && "hover:scale-110 transition-all"}`}
     >
       {info.company.name}
     </a>
